407d33e4d4636bf63d064d4fe4704a4d7f6b191b
Babatunde Alli <tunsmart@gmail.com>
2022-02-06 23:14:06 -0500

    Finish Caesar Cipher

---
+++ caesar_cipher.rb
@@ -0,0 +1,43 @@
+def is_Upcase? (chr)
+    chr == chr.upcase
+end
+
+def return_new_letter (chr, new_letter)
+    if is_Upcase?(chr)
+        return new_letter.upcase
+    else
+        return new_letter
+    end
+end
+
+def create_output_array(input_array, shift_factor, letters)
+    return if shift_factor > 25
+    arr = input_array.map do |chr|
+        if chr.match?(/[[:alpha:]]/)
+            new_letter_position = letters.index(chr.downcase) + shift_factor
+            if new_letter_position > 25
+                new_letter_position2 = new_letter_position - 26
+                new_letter = letters[new_letter_position2]
+                return_new_letter(chr, new_letter)
+            else
+                new_letter = letters[new_letter_position]
+                return_new_letter(chr, new_letter)
+            end
+        else
+            chr
+        end
+    end
+    return arr
+end
+
+def caesar_cipher(string, shift_factor)
+    return "Error! Shift factor cannot be greater than 25" if shift_factor > 25
+    letters = ('a'..'z').to_a
+    input_array = string.split('')
+    output_array = create_output_array(input_array, shift_factor, letters)
+
+    return output_array.join('')
+    
+end
+
+p caesar_cipher("I am Julius^ Ceasar^", 25)
